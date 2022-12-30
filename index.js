b9030ac1f6709af6624615d62955c0b602df59c1
aknathweb <aniknwu.cse@gmail.com>
2022-12-30 22:46:36 +0600

    handle login user information

--- index.js
+++ index.js
@@ -24,7 +24,27 @@ async function run() {
         const userCollection = client.db('MirrorChat').collection('Users');
         const postCollection = client.db('MirrorChat').collection('Posts');
         const commentCollection = client.db('MirrorChat').collection('Comments');
-
+        app.post('/user', async (req, res) => {
+            // name,email,photo,university,address
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.get('/user', async (req, res) => {
+            const searchEmail = req.query.email;
+            console.log(searchEmail)
+            const query = { email: searchEmail };
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
 
 
 
